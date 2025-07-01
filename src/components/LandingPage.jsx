1b2c900b8814551c3ff429f78020586bb8220974
Bill <bill.omondi@student.moringaschool.com>
2025-07-01 11:49:27 +0300

    Interface change

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 function LandingPage( {navigateTo} ) {
   return (
     <div className='landing-page'>
-      <h1>Welcome to harambee system</h1>
+      <h1>Join or Start an Harambee </h1>
       <div className='options'>
         <div className='option-card-1' onClick={() => navigateTo('orgRegistration')}>
-          <h3>Interested in Starting an Harambee?</h3>
+          <h3>Start An Harambee</h3>
           <p><strong>Register </strong>or <strong>Login</strong> your organization</p>
         </div>
         <div className='option-card-2' onClick={() => navigateTo('userPayment')}>
-          <h3>For Donors</h3>
-          <p>Proceed to make a donation</p>
+          <h3>Join an Harambee</h3>
+          <p>For Donors</p>
         </div>
       </div>
     </div>
